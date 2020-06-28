556370fe6deb58313dd37bb1d08e8ad892124f96
Craig Van Wiechen <craig.vanwiechen@gmail.com>
2020-06-28 20:53:22 +0000

    Function Examples

---
+++ hello.js
@@ -0,0 +1,27 @@
+/* Function */
+
+// const sayHello  = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+
+/* Function */
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Miranda");
+
+/* Console */
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+/* Return */
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
